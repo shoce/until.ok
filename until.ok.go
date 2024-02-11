576d6a0e1d0bbc400948d3ce0104548a498ad188
shoce <yogze@icloud.com>
2024-02-11 18:42:15 +0530

    StopAfter

--- until.ok.go
+++ until.ok.go
@@ -25,11 +25,16 @@ const (
 
 func main() {
 	var err error
+
 	var Duration time.Duration
+	var StopAfter time.Duration
+
+	var cmd string
+	var args []string
 	var Command *exec.Cmd
 
-	if len(os.Args) < 3 {
-		fmt.Fprintf(os.Stderr, "usage: until.ok duration command"+NL)
+	if len(os.Args) < 4 || (os.Args[2] != "--" && os.Args[3] != "--") {
+		fmt.Fprintf(os.Stderr, "usage: until.ok duration [stopafter] -- command [args]"+NL)
 		os.Exit(1)
 	}
 
@@ -39,8 +44,29 @@ func main() {
 		os.Exit(1)
 	}
 
+	if os.Args[2] != "--" {
+		StopAfter, err = time.ParseDuration(os.Args[2])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "time.ParseDuration %s: %v"+NL, os.Args[2], err)
+			os.Exit(1)
+		}
+	}
+
+	if os.Args[2] == "--" {
+		cmd = os.Args[3]
+		args = os.Args[4:]
+	} else if os.Args[3] == "--" {
+		cmd = os.Args[4]
+		args = os.Args[5:]
+	} else {
+		fmt.Fprintf(os.Stderr, "error: there must be '--' before the command"+NL)
+		os.Exit(1)
+	}
+
+	StartTime := time.Now()
+
 	for {
-		Command = exec.Command(os.Args[2], os.Args[3:]...)
+		Command = exec.Command(cmd, args...)
 		Command.Stdin, Command.Stdout, Command.Stderr = os.Stdin, os.Stdout, os.Stderr
 		fmt.Fprintf(os.Stderr, NL+"%s:"+NL, Command)
 		err = Command.Run()
@@ -53,6 +79,11 @@ func main() {
 
 		fmt.Fprintf(os.Stderr, NL+"sleeping %v"+NL, Duration)
 		time.Sleep(Duration)
+
+		if StopAfter > 0 && time.Now().Sub(StartTime) > StopAfter {
+			fmt.Fprintf(os.Stderr, NL+"stopping after %v"+NL, StopAfter)
+			break
+		}
 	}
 
 }
